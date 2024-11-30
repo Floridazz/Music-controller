b450dfef1474409a585f89f7bd3866accdba6ed8
QuocAn <anquoc8201@gmail.com>
2024-11-30 20:35:22 +0700

    final fix

--- frontend/src/components/Room.js
+++ frontend/src/components/Room.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Button, Typography } from "@material-ui/core";
 import CreateRoomPage from "./CreateRoomPage";
+import MusicPlayer from "./MusicPlayer";
 
 export default class Room extends Component {
   constructor(props) {
@@ -10,6 +11,8 @@ export default class Room extends Component {
       guestCanPause: false,
       isHost: false,
       showSettings: false,
+      spotifyAuthenticated: false,
+      song: {},
     };
     this.roomCode = this.props.match.params.roomCode;
     this.leaveButtonPressed = this.leaveButtonPressed.bind(this);
@@ -17,7 +20,17 @@ export default class Room extends Component {
     this.renderSettingsButton = this.renderSettingsButton.bind(this);
     this.renderSettings = this.renderSettings.bind(this);
     this.getRoomDetails = this.getRoomDetails.bind(this);
+    this.authenticateSpotify = this.authenticateSpotify.bind(this);
+    this.getCurrentSong = this.getCurrentSong.bind(this);
     this.getRoomDetails();
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(this.getCurrentSong, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
   }
 
   getRoomDetails() {
@@ -35,8 +48,43 @@ export default class Room extends Component {
           guestCanPause: data.guest_can_pause,
           isHost: data.is_host,
         });
+        if (this.state.isHost) {
+          this.authenticateSpotify();
+        }
       });
   }
+
+  authenticateSpotify() {
+    fetch("/spotify/is-authenticated")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ spotifyAuthenticated: data.status });
+        console.log(data.status);
+        if (!data.status) {
+          fetch("/spotify/get-auth-url")
+            .then((response) => response.json())
+            .then((data) => {
+              window.location.replace(data.url);
+            });
+        }
+      });
+  }
+
+  getCurrentSong() {
+    fetch("/spotify/current-song")
+      .then((response) => {
+        if (!response.ok) {
+          return {};
+        } else {
+          return response.json();
+        }
+      })
+      .then((data) => {
+        this.setState({ song: data });
+        console.log(data);
+      });
+  }
+
 
   leaveButtonPressed() {
     const requestOptions = {
@@ -58,7 +106,7 @@ export default class Room extends Component {
   renderSettings() {
     return (
       <Grid container spacing={1}>
-        <Grid item xs={12} align='center'>
+        <Grid item xs={12} align="center">
           <CreateRoomPage
             update={true}
             votesToSkip={this.state.votesToSkip}
@@ -67,7 +115,7 @@ export default class Room extends Component {
             updateCallback={this.getRoomDetails}
           />
         </Grid>
-        <Grid item xs={12} align='center'>
+        <Grid item xs={12} align="center">
           <Button
             variant="contained"
             color="secondary"
@@ -77,7 +125,7 @@ export default class Room extends Component {
           </Button>
         </Grid>
       </Grid>
-    )
+    );
   }
 
   renderSettingsButton() {
@@ -96,7 +144,7 @@ export default class Room extends Component {
 
   render() {
     if (this.state.showSettings) {
-      return this.renderSettings()
+      return this.renderSettings();
     }
     return (
       <Grid container spacing={1}>
@@ -105,21 +153,7 @@ export default class Room extends Component {
             Code: {this.roomCode}
           </Typography>
         </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Votes: {this.state.votesToSkip}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Guest Can Pause: {this.state.guestCanPause.toString()}
-          </Typography>
-        </Grid>
-        <Grid item xs={12} align="center">
-          <Typography variant="h6" component="h6">
-            Host: {this.state.isHost.toString()}
-          </Typography>
-        </Grid>
+        <MusicPlayer {...this.state.song} />
         {this.state.isHost ? this.renderSettingsButton() : null}
         <Grid item xs={12} align="center">
           <Button
--- frontend/urls.py
+++ frontend/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from .views import index
 
-app_name = "frontend"
+app_name = 'frontend'
 
 urlpatterns = [
-    path("", index, name=""),
-    path("join", index),
-    path("create", index),
-    path("join/1", index),
-    path("room/<str:roomCode>", index),
+    path('', index, name=''),
+    path('join', index),
+    path('create', index),
+    path('room/<str:roomCode>', index)
 ]
